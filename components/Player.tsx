fa99352b056d7a14b4db6844b871938d7cb6360b
Filippo <filippo.imarisio@validators.nl>
2023-11-09 17:43:17 +0100

    Player's menu outline and temp counter

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,17 +1,69 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import PlayerMenu from './PlayerMenu';
 
 export default function Player() {
 
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(20);
+  const [tempCounter, setTempCounter] = useState(0);
+  const [showTempCounter, setShowTempCounter] = useState(false)
+
+  enum Operations {
+    plus = 'plus',
+    minus = 'minus'
+  }
+
+  type ValueOf<T> = T[keyof T];
+
+  const displayTempCounter = () => {
+    setShowTempCounter(true)
+    setTimeout(()=> {
+      setShowTempCounter(false)
+      setTempCounter(0)
+    }, 3000)
+  }
+
+  const handleCounterInteraction = (operation: ValueOf<typeof Operations>) => {
+    if (operation === Operations.plus) {
+      setCounter(counter + 1)
+      setTempCounter(tempCounter + 1)
+    }
+    if (operation === Operations.minus) {
+      setCounter(counter - 1)
+      setTempCounter(tempCounter - 1)
+    }
+    if (!showTempCounter) displayTempCounter()
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.tempCounterWrapper}>
+        <Text style={[styles.tempCounter, !showTempCounter && styles.hide]}>{tempCounter}</Text>
+      </View>
       <View style={[styles.container, styles.counter]}>
-        <TouchableOpacity onPress={()=>setCounter(counter - 1)} style={styles.counterButton}><Text>-</Text></TouchableOpacity>
-          <Text style={styles.counterAmount}>{counter}</Text>
-        <TouchableOpacity onPress={()=>setCounter(counter + 1)} style={styles.counterButton}><Text>+</Text></TouchableOpacity>
+        <TouchableOpacity onPress={()=>handleCounterInteraction(Operations.minus)} style={styles.counterButton}>
+          <Image
+            source={require('../assets/minus-circle-outline.png')}
+            resizeMode = 'contain'
+            style= {{
+              height: 40,
+              width: 40,
+              tintColor: 'black'
+            }}
+          />
+        </TouchableOpacity>
+        <Text style={styles.counterAmount}>{counter}</Text>
+        <TouchableOpacity onPress={()=>handleCounterInteraction(Operations.plus)} style={styles.counterButton}>
+          <Image
+            source={require('../assets/plus-circle-outline.png')}
+            resizeMode = 'contain'
+            style= {{
+              height: 40,
+              width: 40,
+              tintColor: 'black'
+            }}
+          />
+        </TouchableOpacity>
       </View>
       <PlayerMenu />
     </View>
@@ -28,13 +80,26 @@ const styles = StyleSheet.create({
   },
   counter: {
     flexDirection: 'row',
-    padding: 40
+    
   },
   counterAmount: {
-    fontSize: 120
+    fontSize: 150
   },
   counterButton: {
-    padding: 20,
-    backgroundColor: 'white'
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  tempCounterWrapper: {
+    flexDirection: 'row',
+    height: 60,
+    alignItems: 'flex-end',
+  },
+  tempCounter: {
+    fontSize: 30
+  },
+  hide: {
+    display: 'none'
   }
 });
--- components/PlayerMenu.tsx
+++ components/PlayerMenu.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Player from './Player';
-import MainMenu from './MainMenu';
+import { StyleSheet, Image, View } from 'react-native';
 
 export default function PlayerMenu() {
   return (
     <View style={styles.container}>
-      <Text>PlayerMenu</Text>
+      <View style={styles.wrapper}>
+        <Image
+          source={require('../assets/mtg-logo.png')}
+          resizeMode = 'contain'
+          style= {{
+            height: 40,
+            width: 40,
+          }}
+        />
+        <Image
+          source={require('../assets/burger-menu.png')}
+          resizeMode = 'contain'
+          style= {{
+            height: 40,
+            width: 40,
+            tintColor: 'black'
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'gold',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '100%'
+    width: '100%',
+    height: 70
   },
+  wrapper: {
+    backgroundColor: 'background: rgba(0, 0, 0, 0.3)',
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 20
+  }
 });
